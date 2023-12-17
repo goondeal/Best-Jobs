0bd58af7dd55f6a046064651833ba8845688d30f
ahmad <ahmed.salamony497@gmail.com>
2023-12-17 08:36:31 +0200

    Start templates

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+from locations.models import Country
 
-# Create your views here.
+
+def index():
+    return render('jobs/index.html')
+
+
+def country_jobs(request, country_code):
+    country = get_object_or_404(Country, pk=country_code)
+    jobs_count = Job.objects.filter(city_state_country__pk=country_code)
+    context = {
+        country: country,
+        jobs_count: jobs_count
+    }
+    return render(request, template_name='jobs/index.html', context=context)
--- project/urls.py
+++ project/urls.py
@@ -3,12 +3,18 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import SignupView
+from jobs.views import index, country_jobs
 
 
 urlpatterns = [
     # auth
     path('signup/', SignupView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
+    
+    # jobs
+    path('', index, name='index'),
+    path('jobs/<str:country_code>', country_jobs, name='country_jobs'),
+
 
     # admin
     path('admin/', admin.site.urls),
