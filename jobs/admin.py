96ef325813db1ba964da2f69b46ec6ebad70d035
ahmad <ahmed.salamony497@gmail.com>
2024-01-27 04:41:08 +0200

    Register jobs.models to admin dashboard

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Job, JobApplication, JobQuestion, JobQuestionAnswer, JobApplicationAnswer
 
-# Register your models here.
+
+admin.site.register(Job)
+admin.site.register(JobApplication)
+admin.site.register(JobQuestion)
+admin.site.register(JobQuestionAnswer)
+admin.site.register(JobApplicationAnswer)
